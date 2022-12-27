3a5229e4afe5d1f750f59d450412582febbe4095
Alex Ustas <alex.ustas@internet.ru>
2022-12-27 10:02:33 +0300

    add get currency from cbr

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telebot
 import bot_api
+import requests as rq
+import xmltodict as xml
 
 # Name: AlexCurrencyRate
 # Bot name: currency_rate_hw10_bot
@@ -15,9 +17,27 @@ description = """
 и так далее."""
 
 
-@bot.message_handler(commands=['start'])
-def game_help(message):
+@bot.message_handler(commands=['start', 'help'])
+def bot_description(message):
     bot.send_message(message.chat.id, description, parse_mode='Markdown')
+
+
+@bot.message_handler(content_types=['text'])
+def get_currency(message):
+    s = rq.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    dct = xml.parse(s.text)['ValCurs']['Valute']
+    curr = message.text[1:].upper()
+    if len(curr) == 3 and message.text[0] == '/':
+        for val in dct:
+            if val['CharCode'] == curr:
+                rate = float(val["Value"].replace(',', '.')) / int(val["Nominal"])
+                bot.send_message(message.chat.id,
+                                 f'Курс валюты {curr} на текущий день: \n{val["Name"]} - {rate}')
+                break
+        else:
+            bot.reply_to(message, 'Валюта не определена')
+    else:
+        bot.reply_to(message, 'Неверный ввод.\nСправка - /help')
 
 
 bot.polling()
